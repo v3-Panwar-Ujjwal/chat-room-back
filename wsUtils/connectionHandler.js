cc38032207d6b7695325d8681fc0a856d2ec9d2a
v3-Panwar-Ujjwal <v3.panwarujjwal@gmail.com>
2024-07-25 19:38:53 +0530

    Add uuid and clientId support

--- wsUtils/connectionHandler.js
+++ wsUtils/connectionHandler.js
@@ -1,10 +1,18 @@
 const { WebSocket } = require("ws");
+const { v4: uuidv4 } = require("uuid");
+
+const clients = new Map();
 
 module.exports = function(wss) {
   wss.on("connection", (ws) => {
     console.log("Cliend connected");
+    const clientId = uuidv4();
+    clients.set(ws, clientId);
+
+    ws.send(JSON.stringify({ type: "init", id: clientId }));
 
     ws.on("close", () => {
+      clients.delete(ws);
       console.log("Client disconnected");
     });
 
@@ -13,6 +21,9 @@ module.exports = function(wss) {
         if (client.readyState === WebSocket.OPEN)
           client.send(data, { binary: isBinary });
       });
+    });
+    ws.on("error", (error) => {
+      console.error(`Client ${clientId} error:`, error);
     });
   });
 };
